e673bf7d19ad921c5237b587adf24245f1fce445
pradanaadn <pradanaadn21@gmail.com>
2025-07-15 22:12:27 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
+
+app = FastAPI()
+
+
+@app.get("/")
+def main():
+    return {"message": "Hello from fastapi-template!"}
+
+
+@app.get("/scalar/docs", include_in_schema=False)
+def scalar_docs():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url, title=app.title
+    )
